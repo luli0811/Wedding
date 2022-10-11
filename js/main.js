901682c317f7970cc8bd23f31b3907730f634b98
Lourdes Sanchez <luli.sanchez0811@gmail.com>
2022-10-11 20:52:31 -0300

    pre-entrega4v0.1

--- js/main.js
+++ js/main.js
@@ -136,3 +136,99 @@ if (eliminar) {
 alert("El envio se hará a:" + sessionStorage.getItem("direccion"))
 alert("Muchas gracias por tu compra " + sessionStorage.getItem("cliente") + ",te esperamos!");
 localStorage.clear();
+
+//PRESUPUESTO
+function tomarDatos() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let correo = document.getElementById("correo").value;
+    let telefonoc = document.getElementById("phone").value;
+    let prov = document.getElementById("provincia").value;
+    let ciudad = document.getElementById("ciud").value;
+    let tamaño = document.getElementById("tamañohojas").value;
+    let cantidadhjbn = document.getElementById("cantidadhojasbn").value;
+    let cantidadhjcolor = document.getElementById("cantidadhojascolor").value;
+    let gruesa = document.getElementById("customRadio1").checked;
+    let fino = document.getElementById("customRadio2").checked;
+    let brillo = document.getElementById("customRadio3").checked;
+    let mate = document.getElementById("customRadio4").checked;
+    if (nombre != "" && apellido != "" && correo != "" && telefonoc != "" && prov != "" && ciudad != "" && tamaño != "" && cantidadhjbn != "" && cantidadhjcolor != "") {
+        let cuenta = 0;
+        if (tamaño == "A4") {
+            if (gruesa) {
+                cuenta = parseInt(cantidadhjbn) * 2 + parseInt(cantidadhjcolor) * 5;
+            } else {
+                if (fino) {
+                    cuenta = parseInt(cantidadhjbn) * 1 + parseInt(cantidadhjcolor) * 2.5;
+                }
+            }
+            if (brillo) {
+                cuenta = cuenta + 100;
+            }
+        }
+
+        if (tamaño == "A3") {
+            if (gruesa) {
+                cuenta = parseInt(cantidadhjbn) * 4 + parseInt(cantidadhjcolor) * 6;
+            } else {
+                if (fino) {
+                    cuenta = parseInt(cantidadhjbn) * 2 + parseInt(cantidadhjcolor) * 3;
+                }
+            }
+            if (brillo) {
+                cuenta = cuenta + 200;
+            }
+        }
+
+
+        if (tamaño == "A5") {
+            if (gruesa) {
+                cuenta = parseInt(cantidadhjbn) * 1 + parseInt(cantidadhjcolor) * 2.5;
+            } else {
+                if (fino) {
+                    cuenta = parseInt(cantidadhjbn) * 0.5 + parseInt(cantidadhjcolor) * 1.25;
+                }
+            }
+            if (brillo) {
+                cuenta = cuenta + 90;
+            }
+        }
+        if (tamaño == "Oficio") {
+            if (gruesa) {
+                cuenta = parseInt(cantidadhjbn) * 2.5 + parseInt(cantidadhjcolor) * 5.5;
+            } else {
+                if (fino) {
+                    cuenta = parseInt(cantidadhjbn) * 1.25 + parseInt(cantidadhjcolor) * 2.25;
+                }
+            }
+            if (brillo) {
+                cuenta = cuenta + 130;
+            }
+        }
+        alert("El total es " + cuenta);
+    } else {
+        alert("Debe completar los campos obligatorios");
+    }
+}
+
+function borrarDatos() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("telefonoc").value = "";
+    document.getElementById("provincia").value = "";
+    document.getElementById("Ciud").value = "";
+    document.getElementById("tamañohojas").value = "";
+    document.getElementById("cantidadhojasbn").value = "";
+    document.getElementById("cantidadhojascolor").value = "";
+    document.getElementById("customRadio1").value = "";
+    document.getElementById("customRadio2").value = "";
+    document.getElementById("customRadio3").value = "";
+    document.getElementById("customRadio4").value = "";
+
+
+}
+
+function calculo() {
+
+}
